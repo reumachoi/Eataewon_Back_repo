9a41b7489b89ef79a5dbe9f372905c3559cd8a01
areum <cunecoder9@gmail.com>
2022-04-13 11:49:54 +0900

    장소선택 결과를 담을 MapResultDto생성

---
+++ eataewon_back/src/main/java/com/proj/eataewon/dto/MapResultDto.java
@@ -0,0 +1,17 @@
+package com.proj.eataewon.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class MapResultDto {
+    private String shopname;
+    private String address;
+    private String shopphnum;
+    private String shopurl;
+    private Double longitude;
+    private Double latitude;
+}
